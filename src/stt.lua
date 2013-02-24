5c075d283251f00a8d86612f60911cf92bb1f8af
Rahul <rahul@serverstack.info>
2013-02-24 03:12:37 +0100

    temp save; stt.lua: gen_stt_frame: handle eth+ipv6+stt_seg+stt_frame headers

--- src/stt.lua
+++ src/stt.lua
@@ -78,7 +78,13 @@ ffi.cdef[[
     } __attribute__((packed));
 ]]
 
-STT_DEST_PORT = 2013 --temporary; this will change as the draft evolves
+STT_DST_PORT = 2013 --temporary; this will change as the draft evolves
+ACK_NUM = 0 --current acknowledgement number (serves as identification)
+
+function init()
+  ACK_NUM = 0
+end
+
 
 --Generate a random source port in the range [49152,65535]
 --Note1: the source port should be constant for each flow
@@ -90,14 +96,6 @@ function gen_src_port(ip_ptr, addr_size, src_port_off, dst_port_off, src_addr_of
   --XXX select a random hash algo at init
 
   -- return 49152 + (CRC-14 of args) 
-end
-
-SEQ_NUM = 0 --current sequence number
-ACK_NUM = 0 --current acknowledgement number
-
-function init()
-  SEQ_NUM = 0
-  ACK_NUM = 0
 end
 
 --determine if given packet is {IPv4,IPv6}/{TCP,UDP}
@@ -129,7 +127,7 @@ function check_packet(mem)
     return ret 
 end
 
---Generate an IPv6+STT frame 
+--Generate an IPv6+STT frame in the given pre-allocated buffer 
 -- stt: stt options - dictionary containing following:
 --      mem, dst_mac, src_mac, src_ip, dst_ip, mss, vlan, ctx_id
 --      mem is of type ffi.cast("struct frame0 *", ...)
@@ -137,15 +135,18 @@ end
 --      ctx_id is of type: uint64_t
 -- pkt: encapsulated packet options - dictionary containing following:
 --      mem, size
---      mem is of type ffi.cast("uint8_t *", ..., size)
+--      mem is of type ffi.cast("uint8_t *", ..., size); -- Read-only
 function gen_stt_frame(stt, pkt)
     assert(stt and pkt, "stt and pkt cannot be nil")
-    assert(pkt.size > 23, "pkt_size should be >23")
 
-    local pm = protected("uint8_t", pkt.mem, 0, 23)
+    local pm = protected("uint8_t", pkt.mem, 0, 78) --14 + 60 + 4
     local ver = bit.band(pm[0], 0x60)
     local proto = nil
-    local hdr_len  = nil -- IP header length
+    local ip_hdr_len = nil
+   -- local hash  = nil
+    local src_port = nil
+    local src_addr_off = nil
+    local addr_len  = nil
 
     assert(stt.mem ~= nil, "stt.mem cannot be nil")
     assert(stt.dst_mac:len() == 6, "dst_mac should have length 6")
@@ -156,6 +157,10 @@ function gen_stt_frame(stt, pkt)
       stt.mem.hdr.eth.src_mac[i-1] = stt.src_mac:ubyte(i) 
     end
 
+    stt.mem.hdr.ipv6.ver_traf_flow = stt.ver_traf_flow or 0x06
+    stt.mem.hdr.ipv6.next_header   = stt.next_header   or 0x06
+    stt.mem.hdr.ipv6.hop_limit     = stt.hop_limit     or 0x40
+
     assert(stt.src_ip:len() == 16, "src_ip should have length 16") 
     assert(stt.dst_ip:len() == 16, "dst_ip should have length 16") 
 
@@ -163,7 +168,7 @@ function gen_stt_frame(stt, pkt)
       stt.mem.hdr.ipv6.dst_ip[i-1] = stt.dst_ip:ubyte(i) 
       stt.mem.hdr.ipv6.src_ip[i-1] = stt.src_ip:ubyte(i) 
     end
-
+    
     stt.mem.stt_hdr.flags  = bits({cs_partial=1}) --gonna be using TSO
     stt.mem.stt_hdr.mss    = stt.mss or assert(false, "stt.mss must be given")
     stt.mem.stt_hdr.vlan   = stt.vlan or assert(false, "stt.vlan must be given")
@@ -172,17 +177,46 @@ function gen_stt_frame(stt, pkt)
     if ver == 0x40 then      --IPv4
         proto = pm[23]     --14 + 9
         stt.mem.stt_hdr.flags  = bits({ipv4=2}, stt.mem.stt_hdr.flags)
-	stt.mem.stt_hdr.l4_ofs = 14 + 4 * bit.band(pm[14], 0x0f) --IHL field
+	ip_hdr_len = 14 + 4 * bit.band(pm[14], 0x0f) --IHL field
+        src_addr_off = 12 --octets
+        addr_len = 4 --octets
+
     elseif ver == 0x60 then  --IPv6
         proto = pm[20]        --14 + 6
-	stt.mem.stt_hdr.l4_ofs = 14 + 40
+	ip_hdr_len = 54       --14 + 40
+        src_addr_off = 8 --octets
+        addr_len = 16 --octets
     else
         assert(false, "Invalid encapsulated packet")
     end
+
+    stt.mem.stt_hdr.l4_ofs = ip_hdr_len
 
     if proto == 0x06 then --TCP
         stt.mem.stt_hdr.flags = bits({tcp=3}, stt.mem.stt_hdr.flags)
     --else hopefully some other valid protocol
     end
+
+    --XXX using CRC-14 hash for now;
+
+    src_port = crc_14(pm[ip_hdr_len], 0xa00)       --source port or ??
+    src_port = crc_14(pm[ip_hdr_len+2], src_port)  --dest port   or ??
+    
+    for i=src_addr_off, src_addr_off + addr_len do
+        src_port = crc_14(pm[i], src_port)
+    end
+
+    for i=src_addr_off + addr_len, src_addr_off + 2 * addr_len do
+        src_port = crc_14(pm[i], src_port)
+    end
+
+    stt.mem.hdr.seg.src_port = 49152 + src_port 
+    stt.mem.hdr.seg.dst_port = STT_DST_PORT
+    stt.mem.hdr.seg.frame_len = 18 + pkt.size
+    stt.mem.hdr.seg.ack_num = ACK_NUM
+
+    stt.mem.hdr.ipv6.paylen = 20 + 18 + pkt.size
+
+    ACK_NUM  = (ACK_NUM + 1) % (2^16)
 
 end
