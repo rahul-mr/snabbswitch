91fc857311c129ccf464bf0bb8f2b7a5ad2ef65d
Rahul <rahul@serverstack.info>
2013-04-04 19:40:09 +0200

    stt.lua:: {init,transmit}: cleanup :-)

--- src/stt.lua
+++ src/stt.lua
@@ -28,18 +28,25 @@ function new()
 	M.nic = nil --nic object from driver's new()
 	M.tx  = { desc=nil, phy=nil, next=nil, total=nil, type="struct stt_tx_frame" } --[Eth+IP+TCP+STT frame header]
 	M.rx  = { desc=nil, phy=nil, next=nil, total=nil, type="struct stt_rx_frame" } --[STT frame header + 64K packet]
-	M.opt = { src={ mac=nil, ip=nil }, dst={ mac=nil, ip=nil }, flags=nil, mss=nil, vlan=nil, ctx_id=nil }
+	M.opt = { eth = { src=nil, dst=nil }, 
+			  ip  = { src=nil, dst=nil, vtf=nil, next=nil, hop=nil }, --vtf = ver + traffic + flow; next = next header
+			  stt = { flag=nil, mss=nil, vlan=nil, ctx=nil } 
+		    }
+	M.flow = nil --'encapsulated' packet flow
 	M.get_random_hash = nil --see init()
 
 	---------------
 	-- FUNCTIONS --
 	---------------
 
+	--Initialization function
+	--options: dictionary containing default values for Eth/IP/STT headers, tx/rx descriptor count
 	function M.init(options)
 		M.ack = 0
 		M.nic = options.nic or assert(false, "stt.lua:: init: options.nic required")
-		M.tx.total = options.tx.total or 10 --total num of descriptors
-		M.rx.total = options.rx.total or 10
+
+		M.tx.total = options.tx_total or 10 --total num of descriptors
+		M.rx.total = options.rx_total or 10
 		M.tx.desc, M.tx.phy = memory.dma_alloc(M.tx.total * ffi.sizeof(M.tx.type))
 		M.rx.desc, M.rx.phy = memory.dma_alloc(M.rx.total * ffi.sizeof(M.rx.type))
 		M.tx.desc = protected(M.tx.type, M.tx.desc, 0, M.tx.total)
@@ -47,73 +54,83 @@ function new()
 		M.tx.next = 0
 		M.rx.next = 0
 
-		M.opt.src.mac = options.src.mac or "\x01\x01\x01\x01\x01\x01"
-		M.opt.dst.mac = options.dst.mac or "\x02\x02\x02\x02\x02\x02"
-		M.opt.src.ip  = options.src.ip  or "\x03\x03\x03\x03\x03\x03\x03\x03\x03\x03\x03\x03\x03\x03\x03\x03"
-		M.opt.dst.ip  = options.dst.ip  or "\x04\x04\x04\x04\x04\x04\x04\x04\x04\x04\x04\x04\x04\x04\x04\x04"
-		M.opt.flags   = options.flags   or bits{cs_partial=1}
-		M.opt.mss     = options.mss     or 1500
-		M.opt.vlan    = options.vlan    or 0
-		M.opt.ctx_id  = options.ctx_id  or 0
+		M.opt.eth.src  = options.eth.src 
+		M.opt.eth.dst  = options.eth.dst 
+		M.opt.ip.src   = options.ip.src  
+		M.opt.ip.dst   = options.ip.dst  
+		M.opt.ip.vtf   = options.ip.vtf  
+		M.opt.ip.next  = options.ip.next
+		M.opt.ip.hop   = options.ip.hop
+		M.opt.stt.flag = options.stt.flag  
+		M.opt.stt.mss  = options.stt.mss    
+		M.opt.stt.vlan = options.stt.vlan   
+		M.opt.stt.ctx  = options.stt.ctx 
 
+		M.flow = {}
 		math.randomseed( tonumber(tostring(os.time()):reverse():sub(1,6)) ) --http://lua-users.org/wiki/MathLibraryTutorial
-		M.get_random_hash = HASH_ALGOS[ math.random(1, #HASH_ALGOS) ] --gen_stt_frame() needs the same hash fn between calls
+		M.get_random_hash = HASH_ALGOS[ math.random(1, #HASH_ALGOS) ] --transmit() needs the same hash fn between calls
 	   																  --to maintain correct 'encapsulated' packet flow
 	end
 
 	M.init()
 
-	--XXX rename to transmit()
-	--Generate an IPv6+STT frame in the given pre-allocated buffer 
-	-- stt: stt options - dictionary containing following:
-	--      mem, dst_mac, src_mac, src_ip, dst_ip, flags, mss, vlan, ctx_id
-	--      mem is of type ffi.cast("struct stt_tx_frame *", ...)
-	--      {src,dst}_{mac,ip} are strings;
-	--      ctx_id is of type: uint64_t
+	-- Transmit the given packet using STT
 	-- pkt: encapsulated packet options - dictionary containing following:
-	--      mem, phy, size
-	--      mem is of type ffi.cast("uint8_t *", ..., size); -- Read-only
-	--      phy is the physical address of the packet buffer (passed to nic)
-	function M.gen_stt_frame(pkt, stt)
-		assert(stt and pkt, "stt and pkt cannot be nil")
+	--      mem: is of type ffi.cast("uint8_t *", ..., size); -- Read-only
+	--      phy: is the physical address of the packet buffer (passed to nic)
+	--      size: is the size of the packet
+	-- options: [optional if configured in init()] 
+	function M.transmit(pkt, options)
+		assert(pkt and pkt.mem and pkt.phy and pkt.size, "pkt is invalid")
+		assert(pkt.size <= 0x10000, "pkt.size must be <= 64K") --max supported pkt.size is 64K
 
-		local pm = protected("uint8_t", pkt.mem, 0, 78) --14 + 60 + 4
+		M.opt.eth.src  = options.eth.src  or M.opt.eth.src  or assert(false, "options.eth.src invalid")
+		M.opt.eth.dst  = options.eth.dst  or M.opt.eth.dst  or assert(false, "options.eth.dst invalid")
+		M.opt.ip.src   = options.ip.src   or M.opt.ip.src   or assert(false, "options.ip.src invalid")
+		M.opt.ip.dst   = options.ip.dst   or M.opt.ip.dst   or assert(false, "options.ip.dst invalid")
+		M.opt.ip.vtf   = options.ip.vtf   or M.opt.ip.vtf   or 0x06
+		M.opt.ip.next  = options.ip.next  or M.opt.ip.next  or 0x06
+		M.opt.ip.hop   = options.ip.hop   or M.opt.ip.hop   or 0x40
+		M.opt.stt.flag = options.stt.flag or M.opt.stt.flag or bits{cs_partial=1} --if TSO used, set cs_partial bit
+		M.opt.stt.mss  = options.stt.mss  or M.opt.stt.mss  or 1422 --1500 - (14 + 40 + 20) - 4
+		M.opt.stt.vlan = options.stt.vlan or M.opt.stt.vlan or 0
+		M.opt.stt.ctx  = options.stt.ctx  or M.opt.stt.ctx  or 0
+
+		assert(M.opt.eth.src:len() == 6, "eth.src should have length 6")
+		assert(M.opt.eth.dst:len() == 6, "eth.dst should have length 6")
+		assert(M.opt.ip.src:len() == 16, "ip.src should have length 16") 
+		assert(M.opt.ip.dst:len() == 16, "ip.dst should have length 16") 
+
+		local pm = protected("uint8_t", pkt.mem, 0, 78) --14 + 60 + 4 [Max: eth + ipv4 + tcp/udp ports] 
 		local ver = bit.band(pm[0], 0x60)
 		local proto = nil
 		local ip_hdr_len = nil
-		local hash = M.get_random_hash()
 		local src_addr_off = nil
 		local addr_len  = nil
+		local hash = M.get_random_hash()
 
-		assert(stt.mem ~= nil, "stt.mem cannot be nil")
-		assert(stt.dst_mac:len() == 6, "dst_mac should have length 6")
-		assert(stt.src_mac:len() == 6, "src_mac should have length 6")
-		
 		for i=1, 6 do
-		  stt.mem.hdr.eth.dst_mac[i-1] = stt.dst_mac:byte(i) 
-		  stt.mem.hdr.eth.src_mac[i-1] = stt.src_mac:byte(i) 
+		  M.tx.desc[M.tx.next].hdr.eth.dst_mac[i-1] = M.opt.eth.dst:byte(i) 
+		  M.tx.desc[M.tx.next].hdr.eth.src_mac[i-1] = M.opt.eth.src:byte(i) 
 		end
 
-		stt.mem.hdr.ipv6.ver_traf_flow = stt.ver_traf_flow or 0x06
-		stt.mem.hdr.ipv6.next_header   = stt.next_header   or 0x06
-		stt.mem.hdr.ipv6.hop_limit     = stt.hop_limit     or 0x40
-
-		assert(stt.src_ip:len() == 16, "src_ip should have length 16") 
-		assert(stt.dst_ip:len() == 16, "dst_ip should have length 16") 
+		M.tx.desc[M.tx.next].hdr.ipv6.ver_traf_flow = M.opt.ip.vtf
+		M.tx.desc[M.tx.next].hdr.ipv6.next_header   = M.opt.ip.next
+		M.tx.desc[M.tx.next].hdr.ipv6.hop_limit     = M.opt.ip.hop
 
 		for i=1, 16 do
-		  stt.mem.hdr.ipv6.dst_ip[i-1] = stt.dst_ip:byte(i) 
-		  stt.mem.hdr.ipv6.src_ip[i-1] = stt.src_ip:byte(i) 
+		  M.tx.desc[M.tx.next].hdr.ipv6.dst_ip[i-1] = M.opt.ip.dst:byte(i) 
+		  M.tx.desc[M.tx.next].hdr.ipv6.src_ip[i-1] = M.opt.ip.src:byte(i) 
 		end
 		
-		stt.mem.stt_hdr.flags  = stt.flags or bits({cs_partial=1}) --if TSO used, set cs_partial bit
-		stt.mem.stt_hdr.mss    = stt.mss or assert(false, "stt.mss must be given")
-		stt.mem.stt_hdr.vlan   = stt.vlan or assert(false, "stt.vlan must be given")
-		stt.mem.stt_hdr.ctx_id = stt.ctx_id or assert(false, "stt.ctx_id must be given")
+		M.tx.desc[M.tx.next].stt_hdr.flags  = M.opt.stt.flag 
+		M.tx.desc[M.tx.next].stt_hdr.mss    = M.opt.stt.mss
+		M.tx.desc[M.tx.next].stt_hdr.vlan   = M.opt.stt.vlan
+		M.tx.desc[M.tx.next].stt_hdr.ctx_id = M.opt.stt.ctx 
 
 		if ver == 0x40 then      --IPv4
 			proto = pm[23]     --14 + 9
-			stt.mem.stt_hdr.flags  = bits({ipv4=2}, stt.mem.stt_hdr.flags)
+			M.tx.desc[M.tx.next].stt_hdr.flags  = bits({ipv4=2}, M.tx.desc[M.tx.next].stt_hdr.flags)
 			ip_hdr_len = 14 + 4 * bit.band(pm[14], 0x0f) --IHL field
 			src_addr_off = 12 --octets
 			addr_len = 4 --octets
@@ -127,10 +144,10 @@ function new()
 			assert(false, "Invalid encapsulated packet")
 		end
 
-		stt.mem.stt_hdr.l4_ofs = ip_hdr_len
+		M.tx.desc[M.tx.next].stt_hdr.l4_ofs = ip_hdr_len
 
 		if proto == 0x06 then --TCP
-			stt.mem.stt_hdr.flags = bits({tcp=3}, stt.mem.stt_hdr.flags)
+			M.tx.desc[M.tx.next].stt_hdr.flags = bits({tcp=3}, M.tx.desc[M.tx.next].stt_hdr.flags)
 		--else hopefully some other valid protocol
 		end
 
@@ -142,16 +159,15 @@ function new()
 			hash.add_byte(pm[src_addr_off + i])
 		end
 
-		stt.mem.hdr.seg.src_port = 49152 + hash.generate() --generate a 14-bit hash
-		stt.mem.hdr.seg.dst_port = STT_DST_PORT
-		stt.mem.hdr.seg.frame_len = 18 + pkt.size --stt frame header + encapsulated packet
-		stt.mem.hdr.seg.ack_num = M.ack
-		stt.mem.hdr.seg.flags = bits({ack=4})
+		M.tx.desc[M.tx.next].hdr.seg.src_port = 49152 + hash.generate() --generate a 14-bit hash
+		M.tx.desc[M.tx.next].hdr.seg.dst_port = STT_DST_PORT
+		M.tx.desc[M.tx.next].hdr.seg.frame_len = 18 + pkt.size --stt frame header + encapsulated packet
+		M.tx.desc[M.tx.next].hdr.seg.ack_num = M.ack
+		M.tx.desc[M.tx.next].hdr.seg.flags = bits{ack=4}
 
-		stt.mem.hdr.ipv6.paylen = 20 + 18 + pkt.size --"TCP-like" header + stt frame header + encapsulated packet
+		M.tx.desc[M.tx.next].hdr.ipv6.paylen = 20 + 18 + pkt.size --TCP-like header + stt frame header + encapsulated packet
 
-		M.ack  = (M.ack + 1) % (2^16)
-
+		M.ack  = (M.ack + 1) % 0x10000 --2^16
 	end
 
 	--returns pointer to element of 'type' located at 'base'+'offset' address (without any protection ;-))
@@ -167,13 +183,12 @@ function new()
 	--Returns: true if relevant headers match
 	--         false otherwise
 	function M.match_headers(a, b, exp_frame_len)
-		local i = 0
+		
 		--check ethernet headers --XXX alternatively use memcmp?
-		while i<=5 do
+		for i=0, 5 do
 			if (a.eth.dst_mac[i] ~= b.eth.dst_mac[i]) or (a.eth.src_mac[i] ~= b.eth.src_mac[i]) then
 				return false
 			end
-			i = i + 1
 		end
 		if a.eth.type ~= b.eth.type then return false end 
 
@@ -184,8 +199,7 @@ function new()
 		   return false
 		end
 
-		i = 0
-		while i<=15 do
+		for i=0, 15 do
 			if (a.ipv6.src_addr[i] ~= b.ipv6.src_addr[i]) or (a.ipv6.dst_addr[i] ~= b.ipv6.dst_addr[i]) then
 				return false
 			end
@@ -206,11 +220,9 @@ function new()
 
 	--XXX convert assert()s to return nil, "error message" ?
 	--Receive a "big" packet using STT
-	--Parameters: buf_address - memory address to copy the received packet
-	--            buf_size    - size of the buffer
 	--Returns: Tuple (length, num_of_packets) [Note: length bytes of given buffer that got used]
 	--            OR (nil, "Error message")
-	function M.receive_stt(nic, buf_address, buf_size)
+	function M.receive(nic, buf_address, buf_size)
 		if nic.rx_empty() then 
 			return nil, "Empty rx ring"
 		else
